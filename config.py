1c4eafd56eafc5f0c79c5781614d94cd37bbc713
Nguyenthinh56 <nguyentrquangthinh12a3@gmail.com>
2025-06-04 20:08:42 +0700

    new version

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ import os
 load_dotenv()
 
 class Config():
-    DEBUG = os.getenv("DEBUG", "False") == "True"
-    SECRET_KEY = os.getenv("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
+    DEBUG = os.getenv("DEBUG", "False").lower() == "True"
+    SECRET_KEY = os.getenv("SECRET_KEY","default-secret-key")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI","sqlite:///default.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
